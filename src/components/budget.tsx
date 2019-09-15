48ecc51dc235916c4756528cebcf541f76d6ac3d
Tom Cumming <mail@tomcumming.co.uk>
2019-09-15 17:50:00 +0100

    Switch budget edit component to hooks

--- src/components/budget.tsx
+++ src/components/budget.tsx
@@ -2,6 +2,240 @@ import * as React from 'react';
 import { Account, Budget } from './app';
 import { startOfDay } from '../date';
 
+export type Props = {
+    initialBudget?: Budget;
+    accounts: { [id: number]: Account };
+    onSave: (budget: Budget) => void;
+    onDelete: () => void;
+};
+
+export default function EditBudget(props: Props) {
+    const [budget, setBudget] = React.useState(
+        budgetFromInitial(props.initialBudget)
+    );
+
+    React.useEffect(() => setBudget(budgetFromInitial(props.initialBudget)), [
+        props.initialBudget
+    ]);
+
+    const onToggleAccount = React.useCallback(
+        makeOnToggleAccount(props.accounts, budget, setBudget),
+        [props.accounts, budget]
+    );
+
+    const onNameChange = React.useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) =>
+            setBudget({
+                ...budget,
+                name: (e.currentTarget as HTMLInputElement).value
+            }),
+        [budget]
+    );
+
+    const onChangeStartingBalance = React.useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) =>
+            setBudget({
+                ...budget,
+                startingBalance: parseFloat(
+                    (e.currentTarget as HTMLInputElement).value
+                )
+            }),
+        [budget]
+    );
+
+    const onChangeTargetBalance = React.useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) =>
+            setBudget({
+                ...budget,
+                targetBalance: parseFloat(
+                    (e.currentTarget as HTMLInputElement).value
+                )
+            }),
+        [budget]
+    );
+
+    const onChangeFirstDay = React.useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) =>
+            setBudget({
+                ...budget,
+                firstDay: (e.currentTarget as HTMLInputElement).value
+            }),
+        [budget]
+    );
+
+    const onChangeLastDay = React.useCallback(
+        (e: React.ChangeEvent<HTMLInputElement>) =>
+            setBudget({
+                ...budget,
+                lastDay: (e.currentTarget as HTMLInputElement).value
+            }),
+        [budget]
+    );
+
+    const onClickSave = React.useCallback(() => {
+        const newBudget: Budget = {
+            ...budget,
+            startingBalance: budget.startingBalance,
+            // These should be start of day anyway
+            firstDay: new Date(budget.firstDay).toJSON(),
+            lastDay: new Date(budget.lastDay).toJSON()
+        };
+        props.onSave(newBudget);
+    }, [props.onSave, budget]);
+
+    const accountToggles = React.useMemo(
+        () => makeAccountToggles(props.accounts, budget, onToggleAccount),
+        [props.accounts, onToggleAccount, budget]
+    );
+
+    const errors = React.useMemo(() => validationMessages(budget), [budget]);
+
+    return (
+        <div className='edit-budget'>
+            <h1>Edit Budget</h1>
+            <fieldset className='flex one'>
+                <label>
+                    <span>Name</span>
+                    <input
+                        type='text'
+                        placeholder='Name'
+                        value={budget.name}
+                        onChange={onNameChange}
+                    />
+                </label>
+            </fieldset>
+            <h2>Accounts</h2>
+            {accountToggles}
+            <fieldset className='flex one'>
+                <label>
+                    <strong>
+                        <span>Starting Balance:</span>
+                    </strong>
+                    <input
+                        type='number'
+                        placeholder='Target Balance'
+                        value={budget.startingBalance}
+                        onChange={onChangeStartingBalance}
+                    />
+                </label>
+            </fieldset>
+            <fieldset className='flex one'>
+                <label>
+                    <strong>
+                        <span>Target Balance:</span>
+                    </strong>
+                    <input
+                        type='number'
+                        placeholder='Target Balance'
+                        value={budget.targetBalance}
+                        onChange={onChangeTargetBalance}
+                    />
+                </label>
+            </fieldset>
+            <fieldset className='flex one'>
+                <label>
+                    <strong>
+                        <span>First Day:</span>
+                    </strong>
+                    <input
+                        type='date'
+                        placeholder='First Day'
+                        value={jsonDateAsInputValue(budget.firstDay)}
+                        onChange={onChangeFirstDay}
+                    />
+                </label>
+            </fieldset>
+            <fieldset className='flex one'>
+                <label>
+                    <strong>
+                        <span>Last Day:</span>
+                    </strong>
+                    <input
+                        type='date'
+                        placeholder='Last Day'
+                        value={jsonDateAsInputValue(budget.lastDay)}
+                        onChange={onChangeLastDay}
+                    />
+                </label>
+            </fieldset>
+            {errors.length > 0 ? (
+                <ul>
+                    {errors.map(error => (
+                        <li key={error}>{error}</li>
+                    ))}
+                </ul>
+            ) : (
+                undefined
+            )}
+            <div className='flex two'>
+                <button
+                    className='success'
+                    onClick={onClickSave}
+                    disabled={errors.length > 0}
+                >
+                    Save
+                </button>
+                <button className='error' onClick={props.onDelete}>
+                    Delete
+                </button>
+            </div>
+        </div>
+    );
+}
+
+function makeAccountToggles(
+    accounts: Props['accounts'],
+    budget: Budget,
+    onToggleAccount: (id: string) => void
+) {
+    return Object.entries(accounts).map(([id, account]) => (
+        <div className='flex one' key={id}>
+            <label>
+                <input
+                    type='checkbox'
+                    checked={budget.accounts.some(aId => aId.toString() === id)}
+                    onChange={() => onToggleAccount(id)}
+                />
+                <span className='checkable'>{account.name}</span>
+            </label>
+        </div>
+    ));
+}
+
+function validationMessages(budget: Budget): string[] {
+    let errors = [];
+
+    if (budget.name.length === 0) errors.push('No name set');
+    if (budget.accounts.length === 0) errors.push('No accounts selected');
+    if (budget.firstDay === '') errors.push('No first day set');
+    if (budget.lastDay === '') errors.push('No last day set');
+    if (
+        budget.firstDay !== '' &&
+        budget.lastDay !== '' &&
+        new Date(budget.firstDay).getTime() > new Date(budget.lastDay).getTime()
+    )
+        errors.push('Last day must be after first day');
+    if (!Number.isFinite(budget.targetBalance))
+        errors.push('Enter a target balance');
+
+    return errors;
+}
+
+function budgetFromInitial(initialBudget?: Budget): Budget {
+    return initialBudget === undefined
+        ? emptyBudget(startOfDay(new Date()))
+        : initialBudget;
+}
+
+const emptyBudget: (date: Date) => Budget = (date: Date) => ({
+    name: '',
+    accounts: [],
+    firstDay: date.toJSON(),
+    lastDay: '',
+    startingBalance: 0,
+    targetBalance: 0
+});
+
 function jsonDateAsInputValue(jsonDate: string) {
     if (jsonDate === '') return '';
     else {
@@ -17,254 +251,24 @@ function jsonDateAsInputValue(jsonDate: string) {
     }
 }
 
-export type Props = {
-    initialBudget?: Budget;
-    accounts: { [id: number]: Account };
-    onSave: (budget: Budget) => void;
-    onDelete: () => void;
-};
-
-const emptyBudget: (date: Date) => Budget = (date: Date) => ({
-    name: '',
-    accounts: [],
-    firstDay: date.toJSON(),
-    lastDay: '',
-    startingBalance: 0,
-    targetBalance: 0
-});
-
-type State = {
-    budget: Budget;
-};
-
-export default class BudgetEdit extends React.PureComponent<Props, State> {
-    constructor(props: Props) {
-        super(props);
-
-        this.state = {
-            budget:
-                props.initialBudget === undefined
-                    ? emptyBudget(startOfDay(new Date()))
-                    : props.initialBudget
-        };
-    }
-
-    onToggleAccount = (id: string) => {
+function makeOnToggleAccount(
+    allAccounts: Props['accounts'],
+    oldBudget: Budget,
+    setBudget: (b: Budget) => void
+) {
+    return (id: string) => {
         const numberId = parseInt(id);
-        const exists = this.state.budget.accounts.some(aId => aId === numberId);
+        const exists = oldBudget.accounts.some(aId => aId === numberId);
         const accounts = exists
-            ? this.state.budget.accounts.filter(aId => aId !== numberId)
-            : this.state.budget.accounts.concat([numberId]);
-        this.setState({
-            budget: {
-                ...this.state.budget,
-                accounts,
-                startingBalance: accounts.reduce(
-                    (p, c) => p + this.props.accounts[c].balance,
-                    0
-                )
-            }
+            ? oldBudget.accounts.filter(aId => aId !== numberId)
+            : oldBudget.accounts.concat([numberId]);
+        setBudget({
+            ...oldBudget,
+            accounts,
+            startingBalance: accounts.reduce(
+                (p, c) => p + allAccounts[c].balance,
+                0
+            )
         });
     };
-
-    onClickSave = () => {
-        const budget: Budget = {
-            ...this.state.budget,
-            startingBalance: this.state.budget.startingBalance,
-            // These should be start of day anyway
-            firstDay: new Date(this.state.budget.firstDay).toJSON(),
-            lastDay: new Date(this.state.budget.lastDay).toJSON()
-        };
-        this.props.onSave(budget);
-    };
-
-    validationMessages(): string[] {
-        const state = this.state.budget;
-
-        let errors = [];
-
-        if (state.name.length === 0) errors.push('No name set');
-        if (state.accounts.length === 0) errors.push('No accounts selected');
-        if (state.firstDay === '') errors.push('No first day set');
-        if (state.lastDay === '') errors.push('No last day set');
-        if (
-            state.firstDay !== '' &&
-            state.lastDay !== '' &&
-            new Date(state.firstDay).getTime() >
-                new Date(state.lastDay).getTime()
-        )
-            errors.push('Last day must be after first day');
-        if (!Number.isFinite(state.targetBalance))
-            errors.push('Enter a target balance');
-
-        return errors;
-    }
-
-    componentDidUpdate(prevProps: Props) {
-        if (this.props.initialBudget !== prevProps.initialBudget) {
-            this.setState({
-                budget:
-                    this.props.initialBudget === undefined
-                        ? emptyBudget(startOfDay(new Date()))
-                        : this.props.initialBudget
-            });
-        }
-    }
-
-    render() {
-        const errors = this.validationMessages();
-
-        return (
-            <div className='edit-budget'>
-                <h1>Edit Budget</h1>
-                <fieldset className='flex one'>
-                    <label>
-                        <span>Name</span>
-                        <input
-                            type='text'
-                            placeholder='Name'
-                            value={this.state.budget.name}
-                            onChange={e =>
-                                this.setState({
-                                    budget: {
-                                        ...this.state.budget,
-                                        name: (e.currentTarget as HTMLInputElement)
-                                            .value
-                                    }
-                                })
-                            }
-                        />
-                    </label>
-                </fieldset>
-                <h2>Accounts</h2>
-                {Object.entries(this.props.accounts).map(([id, account]) => (
-                    <div className='flex one' key={id}>
-                        <label>
-                            <input
-                                type='checkbox'
-                                checked={this.state.budget.accounts.some(
-                                    aId => aId.toString() === id
-                                )}
-                                onChange={_ => this.onToggleAccount(id)}
-                            />
-                            <span className='checkable'>{account.name}</span>
-                        </label>
-                    </div>
-                ))}
-                <fieldset className='flex one'>
-                    <label>
-                        <strong>
-                            <span>Starting Balance:</span>
-                        </strong>
-                        <input
-                            type='number'
-                            placeholder='Target Balance'
-                            value={this.state.budget.startingBalance}
-                            onChange={e =>
-                                this.setState({
-                                    budget: {
-                                        ...this.state.budget,
-                                        startingBalance: parseFloat(
-                                            (e.currentTarget as HTMLInputElement)
-                                                .value
-                                        )
-                                    }
-                                })
-                            }
-                        />
-                    </label>
-                </fieldset>
-                <fieldset className='flex one'>
-                    <label>
-                        <strong>
-                            <span>Target Balance:</span>
-                        </strong>
-                        <input
-                            type='number'
-                            placeholder='Target Balance'
-                            value={this.state.budget.targetBalance}
-                            onChange={e =>
-                                this.setState({
-                                    budget: {
-                                        ...this.state.budget,
-                                        targetBalance: parseFloat(
-                                            (e.currentTarget as HTMLInputElement)
-                                                .value
-                                        )
-                                    }
-                                })
-                            }
-                        />
-                    </label>
-                </fieldset>
-                <fieldset className='flex one'>
-                    <label>
-                        <strong>
-                            <span>First Day:</span>
-                        </strong>
-                        <input
-                            type='date'
-                            placeholder='First Day'
-                            value={jsonDateAsInputValue(
-                                this.state.budget.firstDay
-                            )}
-                            onChange={e =>
-                                this.setState({
-                                    budget: {
-                                        ...this.state.budget,
-                                        firstDay: (e.currentTarget as HTMLInputElement)
-                                            .value
-                                    }
-                                })
-                            }
-                        />
-                    </label>
-                </fieldset>
-                <fieldset className='flex one'>
-                    <label>
-                        <strong>
-                            <span>Last Day:</span>
-                        </strong>
-                        <input
-                            type='date'
-                            placeholder='Last Day'
-                            value={jsonDateAsInputValue(
-                                this.state.budget.lastDay
-                            )}
-                            onChange={e =>
-                                this.setState({
-                                    budget: {
-                                        ...this.state.budget,
-                                        lastDay: (e.currentTarget as HTMLInputElement)
-                                            .value
-                                    }
-                                })
-                            }
-                        />
-                    </label>
-                </fieldset>
-                {errors.length > 0 ? (
-                    <ul>
-                        {errors.map(error => (
-                            <li key={error}>{error}</li>
-                        ))}
-                    </ul>
-                ) : (
-                    undefined
-                )}
-                <div className='flex two'>
-                    <button
-                        className='success'
-                        onClick={this.onClickSave}
-                        disabled={errors.length > 0}
-                    >
-                        Save
-                    </button>
-                    <button className='error' onClick={this.props.onDelete}>
-                        Delete
-                    </button>
-                </div>
-            </div>
-        );
-    }
 }
