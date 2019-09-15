b1ff301babb25cf231af068a093d3d72e26290b5
Tom Cumming <mail@tomcumming.co.uk>
2019-09-15 18:04:33 +0100

    Move settings component to hooks

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -7,62 +7,58 @@ export type Props = {
     setStoredState: (storedState: StoredState) => void;
 };
 
-type State = {};
-
-export default class Settings extends React.PureComponent<Props, State> {
-    createDataString = () => {
-        const blob = new Blob(
-            [JSON.stringify(this.props.storedState, undefined, 2)],
-            {
-                type: 'application/json'
+export default function Settings(props: Props) {
+    const onFileInputChange = React.useCallback(
+        (e: React.FormEvent) => {
+            const input: HTMLInputElement = e.currentTarget as HTMLInputElement;
+            const file = input.files !== null ? input.files[0] : undefined;
+            if (file !== undefined) {
+                const fileReader = new FileReader();
+                fileReader.onload = e => {
+                    const json = (e.target as any).result;
+                    const storedState: StoredState = JSON.parse(json);
+                    input.value = '';
+                    props.setStoredState(storedState);
+                };
+                fileReader.readAsText(file);
             }
-        );
+        },
+        [props.setStoredState]
+    );
 
-        return URL.createObjectURL(blob);
-    };
+    const onDelete = React.useCallback(() => {
+        if (confirm('Are you sure')) props.setStoredState(initialStoredState);
+    }, [props.setStoredState]);
 
-    onFileInputChange = (e: React.FormEvent) => {
-        const input: HTMLInputElement = e.currentTarget as HTMLInputElement;
-        const file = input.files !== null ? input.files[0] : undefined;
-        if (file !== undefined) {
-            const fileReader = new FileReader();
-            fileReader.onload = e => {
-                const json = (e.target as any).result;
-                const storedState: StoredState = JSON.parse(json);
-                input.value = '';
-                this.props.setStoredState(storedState);
-            };
-            fileReader.readAsText(file);
-        }
-    };
+    const dataString = React.useMemo(
+        () => createDataString(props.storedState),
+        [props.storedState]
+    );
 
-    onDelete = () => {
-        if (confirm('Are you sure'))
-            this.props.setStoredState(initialStoredState);
-    };
-
-    render() {
-        return (
-            <div className='settings'>
-                <div className='flex one'>
-                    <a
-                        className='button'
-                        href={this.createDataString()}
-                        download='budget.json'
-                    >
-                        Export Data
-                    </a>
-                    <p>Import Data:</p>
-                    <input
-                        type='file'
-                        accept='.json'
-                        onChange={this.onFileInputChange}
-                    />
-                    <button className='error' onClick={this.onDelete}>
-                        Reset Local Data
-                    </button>
-                </div>
+    return (
+        <div className='settings'>
+            <div className='flex one'>
+                <a className='button' href={dataString} download='budget.json'>
+                    Export Data
+                </a>
+                <p>Import Data:</p>
+                <input
+                    type='file'
+                    accept='.json'
+                    onChange={onFileInputChange}
+                />
+                <button className='error' onClick={onDelete}>
+                    Reset Local Data
+                </button>
             </div>
-        );
-    }
+        </div>
+    );
+}
+
+function createDataString(storedState: StoredState) {
+    const blob = new Blob([JSON.stringify(storedState, undefined, 2)], {
+        type: 'application/json'
+    });
+
+    return URL.createObjectURL(blob);
 }
